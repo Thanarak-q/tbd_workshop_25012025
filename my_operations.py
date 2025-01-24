5d52807f681a56985ae6cb14a0b48134cb182bb2
thanarak-q <thanarak_ka@cmu.ac.th>
2025-01-25 06:47:36 +0700

    create greeting from ab

--- my_operations.py
+++ my_operations.py
@@ -1,7 +1,7 @@
 # Member A: edit this function.
 def add(num1: int, num2: int) -> None:
     # Edit code under this line
-    
+    res = num1 + num2
     print(num1, "+", num2, "is", res)
     pass
 
@@ -9,7 +9,7 @@ def add(num1: int, num2: int) -> None:
 # Member B: edit this function.
 def diff(num1: int, num2: int) -> None:
     # Edit code under this line
-
+    res = num1 - num2
     print(num1, "-", num2, "is", res)
     pass
 
@@ -17,6 +17,6 @@ def diff(num1: int, num2: int) -> None:
 # Member C: edit this function.
 def mul(num1: int, num2: int) -> None:
     # Edit code under this line
-
+    res = num1 * num2
     print(num1, "*", num2, "is", res)
     pass
