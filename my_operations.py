5fdd3027e88db62fb20a73ad0bc74bbf9c758312
thanarak-q <thanarak_ka@cmu.ac.th>
2025-01-25 06:51:31 +0700

    delete step 3 from a

--- my_operations.py
+++ my_operations.py
@@ -1,11 +1,6 @@
 # Member A: edit this function.
 def add(num1: int, num2: int) -> None:
     # Edit code under this line
-<<<<<<< HEAD
-    res = num1 + num2
-=======
-
->>>>>>> 9b2e11491651ef8adc0c37f2e9ffe5a02eac3fde
     print(num1, "+", num2, "is", res)
     pass
 
@@ -13,7 +8,6 @@ def add(num1: int, num2: int) -> None:
 # Member B: edit this function.
 def diff(num1: int, num2: int) -> None:
     # Edit code under this line
-    res = num1 - num2
     print(num1, "-", num2, "is", res)
     pass
 
@@ -21,6 +15,5 @@ def diff(num1: int, num2: int) -> None:
 # Member C: edit this function.
 def mul(num1: int, num2: int) -> None:
     # Edit code under this line
-    res = num1 * num2
     print(num1, "*", num2, "is", res)
     pass
